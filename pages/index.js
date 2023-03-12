807c00ad38b0e647d4a1af24c20ef733a83cb27b
seni95 <ghkdtpdus95@naver.com>
2023-03-12 20:55:24 +0900

    applied routing function on web

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,33 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Seo from "../component/Seo";
 
 
 export default function Home({results}) {
+  const router = useRouter();
+  const onClick = (id,title)=>{
+    // router.push(`/api/movies/${id}`);
+    //일반적인 라우팅 방식
+
+    router.push({
+      pathname:`/movies/${id}`,
+      query:{
+        title
+      }
+    },`/movies/${id}`)
+  }
+
   return (
     <div className="container">
       <Seo title="Home" />
       {results?.map((movie) => (
-        <div className="movie" key={movie.id}>
+        // <Link  href={`/movies/${movie.id}`} key={movie.id}>
+        <div onClick={()=>onClick(movie.id,movie.original_title)} className="movie" key={movie.id}>
             <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
         </div>
+        // </Link>
       ))}
        <style jsx>{`
         .container {
@@ -24,6 +41,9 @@ export default function Home({results}) {
           border-radius: 12px;
           transition: transform 0.2s ease-in-out;
           box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie{
+            cursor:pointer;
         }
         .movie:hover img {
           transform: scale(1.05) translateY(-10px);
